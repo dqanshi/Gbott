63f4c1946a55f22df24634cba2cdeb03c3f78676
SOUL々H҉A҉C҉K҉E҉R҉ <63580147+dqanshi@users.noreply.github.com>
2025-03-15 23:07:54 +0530

    Update start.py

--- Emilia/pyro/start/start.py
+++ Emilia/pyro/start/start.py
@@ -50,14 +50,15 @@ async def starttt(client, message):
                     InlineKeyboardButton("OWNER", url=f"https://t.me/am_dq_fan"),],            
             ]
             
-        await message.reply_photo(
-            photo=DEWMI_IMG,
-            caption=START_TEXT.format(escape_markdown(first_name), BOT_NAME),
-            reply_markup=InlineKeyboardMarkup(buttons),
+           await message.reply_photo(
+               photo=DEWMI_IMG,
+               caption=START_TEXT.format(escape_markdown(first_name), BOT_NAME),
+               reply_markup=InlineKeyboardMarkup(buttons),
     )
 
         elif message.chat.type != ChatType.PRIVATE:
             await message.reply("Hey there, ping me in my PM to get help!")
+
 
     if len(message.text.split()) > 1:
         user = message.from_user.id
