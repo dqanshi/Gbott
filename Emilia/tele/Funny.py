e3115a5befb3e7b1ba6e1c30eeea2a0bed2afd7b
SOUL々H҉A҉C҉K҉E҉R҉ <63580147+dqanshi@users.noreply.github.com>
2025-03-29 18:39:15 +0530

    Update Funny.py

--- Emilia/tele/Funny.py
+++ Emilia/tele/Funny.py
@@ -5,16 +5,16 @@ from telethon import events
 from Emilia import telethn, ORIGINAL_EVENT_LOOP
 
 loha = [
-"odra",
-"poda",
-"kilavan",
-"kilavi",
-"run",
-"mari iri angot",
-"thenga",
-"vazha",
-"mandi",
-"pottan"
+"She was beautiful, not like those girls in the magazines. She was beautiful for the way she thought.",
+"Her soul is like a diamond, beautiful and unbreakable.",
+"You are a rare gem, a single soul in a world of billions.",
+"Her smile is the most captivating beauty.",
+"She is the embodiment of grace, charm, and beauty.",
+"Her love for others makes her even more beautiful.",
+"She walks in beauty, like the night.",
+"Her inner beauty captivates and enchants.",
+"Her laughter is an orchestra of beauty in its purest form.",
+"A strong woman looks a challenge in the eye and gives it a wink."
     ]
 
 
